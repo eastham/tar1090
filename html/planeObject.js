8fa5084002cb311d58b42e6d3879313250cb819c
Paul Eastham <eastham@gmail.com>
2024-04-16 21:11:42 -0700

    Tweak truck naming

--- html/planeObject.js
+++ html/planeObject.js
@@ -1719,7 +1719,12 @@ PlaneObject.prototype.updateMarker = function(moved) {
     icaonum <= reserved_icao_start + reserved_icao_end
   ) {
     icaoType = "SERV";
-    this.flight = "OPS" + (icaonum - reserved_icao_start + 1);
+    vehicleNum = icaonum - reserved_icao_start
+    if (vehicleNum == 0) {
+        this.flight = "HP_1";
+    } else {
+        this.flight = "TRUCK_" + vehicleNum;
+    }
   }
   // ****** end 88NV mods  ******
 
