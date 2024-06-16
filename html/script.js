76d048a488470d57dbc3bd0acc79ff2daadda435
Paul Eastham <eastham@gmail.com>
2024-06-15 20:36:12 -0700

    Revert "AD-336 Update Globe UI icons and add replay functionality (#13)"
    This reverts commit dcb524c23cd385243c04d481c1b681abd17290a6.

--- html/script.js
+++ html/script.js
@@ -186,7 +186,6 @@ let badDot;
 let badDotMlat;
 
 let showingReplayBar = false;
-let replayShouldPlayOnFirstLoad = true;
 
 function processAircraft(ac, init, uat) {
     const isArray = Array.isArray(ac);
@@ -1016,7 +1015,6 @@ function initPage() {
         }
         console.log(ts);
         replay = replayDefaults(ts);
-        replayShouldPlayOnFirstLoad = false;
     }
 
     //Pulling filters from params
@@ -1176,7 +1174,6 @@ function initPage() {
 
     jQuery('#settingsCog').on('click', function() {
         jQuery('#settings_infoblock').toggle();
-        $(this).toggleClass('settingsCog-active');
     });
 
     if (onMobile) {
@@ -1193,7 +1190,6 @@ function initPage() {
 
     jQuery('#settings_close').on('click', function() {
         jQuery('#settings_infoblock').hide();
-        jQuery('#settingsCog').removeClass('settingsCog-active');
     });
 
     jQuery('#groundvehicle_filter').on('click', function() {
@@ -7402,7 +7398,7 @@ function replayDefaults(ts) {
         playing: true,
         ts: ts,
         ival: 60 * 1000,
-        speed: 10,
+        speed: 30,
         dateText: zDateString(ts),
         hours: ts.getUTCHours(),
         minutes: ts.getUTCMinutes(),
@@ -7628,7 +7624,6 @@ function replaySetTimeHint(arg) {
 
     let minutes = replay.ts.getUTCMinutes();
     jQuery('#minuteSelect').slider("option", "value", minutes);
-
     replayJumpEnabled = true;
 }
 
@@ -7949,32 +7944,13 @@ function playReplay(state){
 function showReplayBar(){
     console.log('showReplayBar()');
     showingReplayBar = !showingReplayBar;
-    // Function that will remove 'replay' token from address bar and reload window
-    const forceReloadWindow = function() {
-        let currentUrl = window.location.href;
-        let url = currentUrl.replace(/replay=[^&]+&?/, '');
-        // Update the URL without reloading the page
-        window.history.pushState({}, '', url);
-        // Reload the page
-        window.location.reload();
-    };
-
     if (!showingReplayBar){
-        // Hide bar
-        jQuery("#RP").removeClass('settingsReplay-active');
         jQuery("#replayBar").hide();
-        // Force-stop playing
-        playReplay(false);
-        // Clear timers
-        replayClear();
         replay = null;
         jQuery('#map_canvas').height('100%');
         jQuery('#sidebar_canvas').height('100%');
         jQuery("#selected_showTrace_hide").show();
-        // Reset everything, as closing things is a bit of a mess
-        forceReloadWindow();
     } else {
-        jQuery("#RP").addClass('settingsReplay-active');
         jQuery("#replayBar").show();
         jQuery("#replayBar").css('display', 'grid');
         jQuery('#replayBar').height('100px');
@@ -8006,39 +7982,28 @@ function showReplayBar(){
         }
 
         jQuery("#replayDatepicker").datepicker(datepickerOptions);
-        let replaySliderHourHandle = jQuery("#replayDatepickerHourHandle");
+
         jQuery('#hourSelect').slider({
             step: 1,
             min: 0,
             max: 23,
-            create: function() {
-                replaySliderHourHandle.text(replay.hours + " hrs");
-            },
             slide: function(event, ui) {
                 replay.hours = ui.value;
-                replaySliderHourHandle.text(ui.value + " hrs");
                 replayOnSliderMove();
             },
-            change: function(event, ui) {
-                replaySliderHourHandle.text(ui.value + " hrs");
+            change: function() {
                 replayJump();
             }
         });
-        let replaySliderMinuteHandle = jQuery("#replayDatepickerMinuteHandle");
         jQuery('#minuteSelect').slider({
             step: 1,
             min: 0,
             max: 59,
-            create: function() {
-                replaySliderMinuteHandle.text(replay.minutes + " min");
-            },
             slide: function(event, ui) {
                 replay.minutes = ui.value;
-                replaySliderMinuteHandle.text(ui.value + " min");
                 replayOnSliderMove();
             },
-            change: function(event, ui) {
-                replaySliderMinuteHandle.text(ui.value + " min");
+            change: function() {
                 replayJump();
             }
         });
@@ -8059,31 +8024,7 @@ function showReplayBar(){
         jQuery('#replaySpeedHint').text('Speed: ' + replay.speed + 'x');
 
         jQuery("#selected_showTrace_hide").hide();
-
-        // Re-adjust sliders to reflect accurate time
-        replayJump();
     }
-    // Function that will wait for replay to start playing
-    const waitForReplay = function(c, max) {
-        if(!replay || replay.playing) {
-            return;
-        }
-        playReplay(true);
-        if(!replay.playing && c < max) {
-            console.log(`[${c}] Wainting for replay to start playing...`);
-            setTimeout(waitForReplay, 500, c+1, max);
-        }
-    };
-    // On very first click, simulate automatic start
-    if(replayShouldPlayOnFirstLoad) {
-        // This starts loading data required to play replay
-        loadReplay(replay.ts);
-        // This will start playing the replay once the data is loaded
-        waitForReplay(0, 20);
-    }
-
-    // Indicate that we've already played once
-    replayShouldPlayOnFirstLoad = false;
 };
 
 function timeoutFetch() {
